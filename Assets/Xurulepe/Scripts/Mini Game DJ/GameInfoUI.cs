138eff07ebab7b184f74853b97d1544553bf68fd
Xurulepe <xurulepe@gmail.com>
2026-05-11 12:00:18 -0300

    Pulso UI text
    + Adicionado um efeito de pulsação nas informações da UI do mini jogo DJ quando as notas são acertadas ou perdidas.

--- Assets/Xurulepe/Scripts/Mini Game DJ/GameInfoUI.cs
+++ Assets/Xurulepe/Scripts/Mini Game DJ/GameInfoUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -5,8 +6,11 @@ public class GameInfoUI : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private TextMeshProUGUI accuracyInfoText;
+    [SerializeField] private RectTransform accuracyInfoRectTransform;
     [SerializeField] private TextMeshProUGUI scoreInfoText;
+    [SerializeField] private RectTransform scoreInfoRectTransform;
     [SerializeField] private TextMeshProUGUI comboCountInfoText;
+    [SerializeField] private RectTransform comboInfoRectTransform;
 
     private void Start()
     {
@@ -16,12 +20,26 @@ public class GameInfoUI : MonoBehaviour
     private void UpdateUI()
     {
         accuracyInfoText.text = GameManager.Instance.AccuracyInfo;
+        Pulse(accuracyInfoRectTransform);
+
         scoreInfoText.text = GameManager.Instance.Score.ToString();
+        Pulse(scoreInfoRectTransform);
+
         comboCountInfoText.text = GameManager.Instance.ComboCount.ToString() + " x";
+        Pulse(comboInfoRectTransform);
     }
 
     private void OnDisable()
     {
         GameManager.Instance.OnScoreChanged -= UpdateUI;        
+    }
+
+    private void Pulse(RectTransform target)
+    {
+        target.DOKill();
+
+        target.localScale = Vector3.one;
+
+        target.DOScale(1.2f, 0.08f).SetLoops(2, LoopType.Yoyo);
     }
 }
