6fbb6e84bff806deb61e298357fa7e26e3ad35b5
Ruan Sarmanho <ruan54382666@edu.pa.senac.br>
2026-07-02 20:46:25 -0300

    add seta indicadora

--- Assets/Ruan/LevelSelect/Script/Cenario/Portas.cs
+++ Assets/Ruan/LevelSelect/Script/Cenario/Portas.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 public class Portas : MonoBehaviour
 {
     public Animator anima;
+    public GameObject Seta;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         anima.SetBool("Abrir", true);
+        Seta.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         anima.SetBool("Abrir", false);
+        Seta.SetActive(false);
+
     }
 }
