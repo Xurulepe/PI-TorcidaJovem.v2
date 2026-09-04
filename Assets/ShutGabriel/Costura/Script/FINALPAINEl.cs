ae1edcf6d59e7b4a58e18e4b636efa3cfb846153
ShutGabriel <gabrielamaciel0307@gmail.com>
2026-09-04 20:08:31 -0300

    UpdateTextura

--- Assets/ShutGabriel/Costura/Script/FINALPAINEl.cs
+++ Assets/ShutGabriel/Costura/Script/FINALPAINEl.cs
@@ -15,10 +15,7 @@ public class FINALPAINEl : MonoBehaviour
 
     private void Start()
     {
-        // Painel começa invisível
         painelTransicao.alpha = 0f;
-
-        // Vestido começa desativado
         vestidoFinal.SetActive(false);
     }
 
@@ -26,29 +23,17 @@ public class FINALPAINEl : MonoBehaviour
     {
         painelTransicao.DOKill();
         spriteVestido.DOKill();
-
-        // Bloqueia cliques durante a transição
         painelTransicao.blocksRaycasts = true;
-
-        // Tela fica preta
         painelTransicao.DOFade(1f, duracaoFade)
             .OnComplete(() =>
             {
-                // Ativa o vestido enquanto a tela está preta
                 vestidoFinal.SetActive(true);
-
-                // Deixa o vestido inicialmente invisível
                 Color cor = spriteVestido.color;
                 cor.a = 0f;
                 spriteVestido.color = cor;
-
-                // Espera
                 DOVirtual.DelayedCall(tempoEspera, () =>
                 {
-                    // Painel preto desaparece
                     painelTransicao.DOFade(0f, duracaoFade);
-
-                    // Vestido aparece gradualmente ⭐
                     spriteVestido.DOFade(1f, duracaoFade)
                         .OnComplete(() =>
                         {
